4bd5131e1a6d7602bcee01c89858d42ff0218a00
trolland97 <trolland97@gmail.com>
2023-03-02 10:39:59 +0100

    Darkmode

--- src/components/CardSlider.js
+++ src/components/CardSlider.js
@@ -4,6 +4,7 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Product from "./Product";
 import { productData, responsive } from "./product_data";
+import logo from '../images/productslogo.png';
 
 export default function CardSlider() {
   const product = productData.map((item) => (
@@ -14,10 +15,20 @@ export default function CardSlider() {
   ));
 
   return (
+    <div className="productsContainer">
+      <div class="productsTitle">
+            <span>Nasi proizvodi</span>
+        </div>
+        <div class="productsDesc"><span>Naše proizvode DVG Cosmetics odlikuje visok kvalitet kojim se dobija kompletna
+                nega lica i tela. Uspeli smo da uskladimo kvalitet i cenu, što su prepoznali i naši kupci.</span></div>
     <div className="CardSlider">
       <Carousel showDots={true} responsive={responsive}>
         {product}
       </Carousel>
+    </div>
+    <div class="productsLogo">
+      <img src={logo} alt='productlogo' />
+        </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,76 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import img from '../images/logo.png';
 
 function Header() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
+  function darkmodeFunction() {
+    const name = document.querySelector(".nameContainer");
+    const brand = document.querySelector(".brandContainer");
+    const distribution = document.querySelector(".distributionContainer");
 
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  window.addEventListener('resize', showButton);
-
+    distribution.classList.toggle("dark-mode");
+    name.classList.toggle("dark-mode");
+    brand.classList.toggle("dark-mode");
+  }
+  
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            TRVL
-            <i class='fab fa-typo3' />
+        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            <img src={img} alt='logo' />
           </Link>
+          <div className='languages'>
+            <ul>
+              <li>
+                <span>Magyar</span>
+                <span>Srpski</span>
+                <span>English</span>
+                <span onClick={darkmodeFunction}><i className="fa-sharp fa-solid fa-moon"></i>DARK MODE</span>
+              </li>
+            </ul>
+          </div>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                Home
+                O NAMA
               </Link>
             </li>
             <li className='nav-item'>
               <Link
-                to='/services'
+                to='/'
                 className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Services
+                onClick={closeMobileMenu}>
+                DVG PROIZVODI
               </Link>
             </li>
             <li className='nav-item'>
               <Link
-                to='/products'
+                to='/'
                 className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Products
+                onClick={closeMobileMenu}>
+                BRENDOVI
               </Link>
             </li>
-
-            <li>
+            <li className='nav-item'>
               <Link
-                to='/sign-up'
-                className='nav-links-mobile'
-                onClick={closeMobileMenu}
-              >
-                Sign Up
+                to='/'
+                className='nav-links'
+                onClick={closeMobileMenu}>
+                DISTRIBUCIJA
               </Link>
             </li>
+            <li className='nav-item'>
+              <Link
+                to='/'
+                className='nav-links'
+                onClick={closeMobileMenu}>
+                SARADNJA
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/'
+                className='nav-links'
+                onClick={closeMobileMenu}>
+                NOVOSTI
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/'
+                className='nav-links'
+                onClick={closeMobileMenu}>
+                KONTAKT
+              </Link>
+            </li>
+            <li className='drop_buttons'>
+                <span onClick={closeMobileMenu}>Magyar</span>
+                <span onClick={closeMobileMenu}>Srpski</span>
+                <span onClick={closeMobileMenu}>English</span>
+                <span onClick={() => {closeMobileMenu();darkmodeFunction();}}><i className="fa-sharp fa-solid fa-moon"></i>DARK MODE</span>
+              </li>
           </ul>
-          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
       </nav>
     </>
